a02342380ecce2c598990ba9cee23c84f62b0d9c
Sandip Ghosh <72227918+sandipghosh18@users.noreply.github.com>
2021-04-12 15:36:25 +0530

    Update structureDemo.cs

--- 12-04-2021/structureDemo.cs
+++ 12-04-2021/structureDemo.cs
@@ -1,1 +1,35 @@
 
+using System.IO;
+using System;
+
+namespace ConsoleApp2
+{
+
+    public struct Employee
+    {
+        public int EmployeeID;
+        public String EmployeeName;
+    }
+
+
+    class Program
+    {
+
+
+        public static void Main()
+        {
+            Employee emp;
+            emp.EmployeeID = 12600;
+            emp.EmployeeName = "Sandip";
+            printEmployeeDeatils(emp);
+            Console.ReadKey();
+
+        }
+
+        public static void printEmployeeDeatils(Employee emp)
+        {
+            Console.WriteLine("Employee Id={0}, Employee Name={1}", emp.EmployeeID, emp.EmployeeName);
+        }
+        
+    }
+}
