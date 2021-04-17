1884abb425183e02e1c25b29d5d483fc42ac1ddc
Sandip Ghosh <72227918+sandipghosh18@users.noreply.github.com>
2021-04-17 12:25:40 +0530

    Create hasTable.cs

---
+++ 17-04-2021/hasTable.cs
@@ -0,0 +1,21 @@
+namespace DemoApplication
+{
+ class Program
+ {
+  static void Main(string[] args)
+  {
+   Hashtable ht = new Hashtable();
+   ht.Add("001",".Net");
+   ht.Add("002","C#");
+   ht.Add("003","ASP.Net");
+ 
+   ICollection keys = ht.Keys;
+ 
+   foreach (String k in keys)
+   {
+    Console.WriteLine(ht[k]);
+   }
+    Console.ReadKey();
+   }
+ }
+} 
