f40e926d540f724ea07fc1b3b50526426db68c0a
Sandip Ghosh <72227918+sandipghosh18@users.noreply.github.com>
2021-04-17 12:29:03 +0530

    Update hasTable.cs

--- 17-04-2021/hasTable.cs
+++ 17-04-2021/hasTable.cs
@@ -1,21 +1,24 @@
+using System;
+using System.Collections;
+
 namespace DemoApplication
 {
- class Program
- {
-  static void Main(string[] args)
-  {
-   Hashtable ht = new Hashtable();
-   ht.Add("001",".Net");
-   ht.Add("002","C#");
-   ht.Add("003","ASP.Net");
- 
-   ICollection keys = ht.Keys;
- 
-   foreach (String k in keys)
-   {
-    Console.WriteLine(ht[k]);
-   }
-    Console.ReadKey();
-   }
- }
-} 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("001", ".Net");
+            ht.Add("002", "C#");
+            ht.Add("003", "ASP.Net");
+
+            ICollection keys = ht.Keys;
+
+            foreach (String k in keys)
+            {
+                Console.WriteLine(ht[k]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
